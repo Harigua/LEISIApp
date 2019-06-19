5f9f555a03ef70c3bb80913839fce54813e31734
MrMaaoui <maaouihariga@gmail.com>
2019-06-19 13:33:51 +0100

    v1.19.06.19-5

--- server.R
+++ server.R
@@ -1492,9 +1492,9 @@ shinyServer(function(input, output,session) {
     
     if( as.character(input$regvisit) ==""){
       info("Error : Missing value Country and state")
-    }else if( !(as.character(input$datedatevisit) <= Sys.Date() && as.character(input$datedatevisit) !="")){
+    }else if( !(as.character(input$datedatevisit) <= Sys.Date()) && as.character(input$datedatevisit) ==""){
       info("Error : Incorrect date Visit Date")
-    }else if( input$dateleavevisit < -1 || input$dateleavevisit == ""){
+    }else if( input$dateleavevisit < (-1) || input$dateleavevisit == ""){
       info("Error : Incorrect value duration")
     }else{
       an.error.occured <- FALSE
